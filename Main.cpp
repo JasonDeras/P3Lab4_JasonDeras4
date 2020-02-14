a60eaf2b5b48d0e0627afd61747304b762a48b7d
JasonDeras <derasjason2@gmail.com>
2020-02-14 15:21:00 -0600

    Creacion de los metodos para agregar libros y usuarios

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+//Variables controladoras
+Cliente ** lista_usuarios = new Cliente *[10];//arreglo de 10 usuarios
+int cantidad_usuarios=0;
+
+Libro ** lista_libros = new Libro *[10];//arreglo de 10 libros
+int cantiad_libros=0;
+
 int menuAdmin(){
 
 	int opcion;
@@ -13,6 +20,7 @@ int menuAdmin(){
 	cout<<"2. Modificar libro"<<endl;
 	cout<<"3. Eliminar libro"<<endl;
 	cout<<"4. Borrar registro de libros"<<endl;
+	cout<<"5. Salir"<<endl;
 	cout<<"Ingrese una opcion: ";
 	cin>>opcion;
 	cout<<"\n\n";
@@ -39,21 +47,80 @@ int menuCliente(){
 }//Fin del menu del cliente
 
 //Opciones del Administrador
-void agregarLibro();
+void agregarLibro(){
+
+    if(cantidad_libros<10){
+
+     	string titulo;
+		string autor;
+		string publicacion;
+		int precio;
+		string estado;
+
+        cout<<"Ingrese nuevo libro"<<endl<<endl;  
+        cout<<"Titulo: ";
+        cin>>tirulo;
+        cout<<"Autor: ";
+        cin>>autor;
+        cout<<"Publicacion: ";
+        cin>>publicacion;
+        cout<<"Precio: ";
+        cin>>precio;
+        cout<<"Estado: ";
+        cin>>estado;
+                  
+        //agregarlo a la lista
+        lista_libros [cantidad_libros] =new Libro(titulo,autor,publicacion,precio,estado);
+        cantidad_lirbos++;         
+        cout<<endl<<"Se agrego el libro perfectamente"<<endl;         
+     }
+     else{
+        cout<<"Ya no hay espacio disponible en el arreglo para guardar libros"<<endl; 
+    }//Fin del if que valida si estan lleno el arreglo de usuarios
+
+}//Fin del metodo para agregar libros
+
 void ModificarLibro();
 int buscarLibro(int);
 void eliminarLibro();
 
 //Void Agregar Usuarios
-void AgregarUsuario();
+void AgregarUsuario(){
+
+    if(cantidad_usuarios<10){
+
+     	string usuario;
+     	string contrasenia;
+     	string nombre;
+     	int edad;
+     	int dinero;
+
+        cout<<"Ingrese nuevo cliente"<<endl<<endl;  
+        cout<<"Usuario: ";
+        cin>>usuario;
+        cout<<"Contraseña: ";
+        cin>>contrasenia;
+        cout<<"Nombre: ";
+        cin>>nombre;
+        cout<<"Edad: ";
+        cin>>edad;
+        cout<<"Dinero: ";
+        cin>>dinero;
+                  
+        //agregarlo a la lista
+        lista_usuarios [cantidad_usuarios] =new Cliente(usuario,contrasenia,nombre,edad,dinero);
+        cantidad_usuarios++;         
+        cout<<endl<<"Se agrego el usuario perfectamente"<<endl;         
+     }
+     else{
+        cout<<"Ya no hay espacio disponible en el arreglo para guardar usuarios"<<endl; 
+    }//Fin del if que valida si estan lleno el arreglo de usuarios
+
+}//Fin del Metodo para agregar usuario
 
 int main(){
 	
-	Cliente ** lista_usuarios = new Cliente* [10];//arreglo de 10 usuarios
-	int cantidad_usuarios=0;
-
-	Libro ** lista_libros = new Libro * [10];//arreglo de 10 libros
-	int cantiad_libros=0;
+	
 
 	lista_usuarios[cantidad_usuarios]=new Cliente("n.duron","sybase","Nicolle Duron",20,1000);
 
@@ -99,7 +166,7 @@ int main(){
    			break;}
 
    			case 2:{
-   				void AgregarUsuario();
+   				AgregarUsuario();
    			break;}
 
    			default:
@@ -111,36 +178,3 @@ int main(){
 	}while(opcion!=3);
 
 }//Fin del main
-
-void AgregarUsuario(){
-
-     if(cantidad_personas<10){
-
-     	string usuario;
-		string contrasenia;
-		string nombre;
-		int edad;
-		int dinero;
-
-        cout<<"Ingrese nuevo cliente"<<endl<<endl;  
-        cout<<"Usuario: ";
-        cin>>usuario;
-        cout<<"Contraseña: ";
-        cin>>contrasenia;
-        cout<<"Nombre: ";
-        cin>>nombre;
-        cout<<"Edad: ";
-        cin>>edad;
-        cout<<"Dinero: ";
-        cin<<dinero;
-                  
-        //agregarlo a la lista
-        lista_usuarios [cantidad_usuarios] =new Cliente(usuario,contrasenia,nombre,edad,dinero);
-        cantidad_usuarios++;         
-        cout<<endl<<"Se agrego el usuario perfectamente"<<endl;         
-     }
-     else{
-        cout<<"Ya no hay espacio disponible en el arreglo para guardar usuarios"<<endl; 
-    }//Fin del if que valida si estan lleno el arreglo de usuarios
-
-}//Fin del Metodo para agregar usuario
